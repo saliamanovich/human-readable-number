08a31ce7207b8be6c736e1f01e0c088f0c6dc46e
saliamanovich <saliamanovicha@gmail.com>
2022-01-05 00:12:24 +0300

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    switch (number){
+        case 1:
+            return 'one';
+            break;
+            default:
+                return 'mistake';
+    }
 }
