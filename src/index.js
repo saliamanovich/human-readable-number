6d715ebe1be7b9fee33c4568c0785258ce501b52
saliamanovich <saliamanovicha@gmail.com>
2022-01-05 01:33:21 +0300

    init: add case 31-99

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-    /*switch (number){
+    switch (number){
         case 1:
             return 'one';
             break;
@@ -57,7 +57,7 @@ module.exports = function toReadable (number) {
 
                 default:
                     //return 'mistake';
-    }*/
+    }
     function showNumber (single_digit) {
         switch (single_digit){
             case 1:
@@ -97,6 +97,41 @@ module.exports = function toReadable (number) {
         let second_number = number.toString().split('').pop();
         let two_number = showNumber(+second_number);
         return `twenty ${two_number}`;
+    }
+    else if (number>30&&number<40){
+        let third_number = number.toString().split('').pop();
+        let three_number = showNumber(+third_number);
+        return `thirty ${three_number}`;
+    }
+    else if (number>40&&number<50){
+        let fourth_number = number.toString().split('').pop();
+        let four_number = showNumber(+fourth_number);
+        return `forty ${four_number}`;
+    }
+    else if (number>50&&number<60){
+        let fifth_number = number.toString().split('').pop();
+        let five_number = showNumber(+fifth_number);
+        return `fifty ${five_number}`;
+    }
+    else if (number>60&&number<70){
+        let sixth_number = number.toString().split('').pop();
+        let six_number = showNumber(+sixth_number);
+        return `sixty ${six_number}`;
+    }
+    else if (number>70&&number<80){
+        let seventh_number = number.toString().split('').pop();
+        let seven_number = showNumber(+seventh_number);
+        return `seventy ${seven_number}`;
+    }
+    else if (number>80&&number<90){
+        let eighth_number = number.toString().split('').pop();
+        let eight_number = showNumber(+eighth_number);
+        return `eighty ${eight_number}`;
+    }
+    else if (number>90&&number<=99){
+        let ninety_number = number.toString().split('').pop();
+        let nine_number = showNumber(+ninety_number);
+        return `ninety ${nine_number}`;
     }
     
     
